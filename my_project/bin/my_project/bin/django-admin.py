f03c551162ac55922f16dab2436328c0aab21a60
Luis Rafael Bernal Torres <luisrafaelbernaltorres@Maclaus2.local>
2017-07-29 01:40:00 +0200

    commit inicial Django

---
+++ my_project/bin/my_project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/luisrafaelbernaltorres/pydjango/my_project/bin/my_project/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
